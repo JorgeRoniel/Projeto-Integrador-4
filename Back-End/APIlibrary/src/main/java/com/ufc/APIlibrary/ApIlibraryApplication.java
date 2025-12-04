bf63df0d41da848104f8ff1b9d878a8183a90df6
JorgeRoniel <jorge.infor4g@gmail.com>
2025-12-04 20:58:32 -0300

    feat/Criação da API | Configuração do CORS

---
+++ Back-End/APIlibrary/src/main/java/com/ufc/APIlibrary/ApIlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ufc.APIlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIlibraryApplication.class, args);
+	}
+
+}
