cda1bb272be71557cbc6ce0ad0f4bb9a049f3109
JorgeRoniel <jorge.infor4g@gmail.com>
2025-12-05 10:28:08 -0300

    feat/ Modificações na Entidade | Criação das Roles | Criação das Migrations | Conexão local com Banco de Dados.

--- Back-End/APIlibrary/src/main/java/com/ufc/APIlibrary/domain/User/User.java
+++ Back-End/APIlibrary/src/main/java/com/ufc/APIlibrary/domain/User/User.java
@@ -1,4 +1,29 @@
 package com.ufc.APIlibrary.domain.User;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "users_tb")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+    private String username;
+    private String name;
+    private String email;
+    private String password;
+    private String phone_number;
+    private UserRoles role;
+
+
 }
--- Back-End/APIlibrary/src/main/java/com/ufc/APIlibrary/repositories/UserRepository.java
+++ Back-End/APIlibrary/src/main/java/com/ufc/APIlibrary/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package com.ufc.APIlibrary.repositories;
 
-public interface UserRepository {
+import com.ufc.APIlibrary.domain.User.User;
+import jakarta.persistence.Column;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
